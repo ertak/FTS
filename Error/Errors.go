a1cf3d54ba3d5e0e985f868cf0028f7f5d4dd430
ugurkapl <ugur_kapl@hotmail.com>
2016-04-13 15:55:45 +0300

    modified

--- Error/Errors.go
+++ Error/Errors.go
@@ -1,66 +1,55 @@
 package Error
 
-
 import (
-"fmt"
-"log"
-"time"
+	"fmt"
+	"log"
+	"time"
 )
+
 //Gerekli Error tanımlamaları buraya eklenecektir.
 
 //Belirtilen dosya bulunamadı! (Daha kompleks yapıya sahiptir.)
 type NotExistFile struct {
-ErrorDescription string
-Path string
-Name string
-
+	ErrorDescription string
+	Path             string
+	Name             string
 }
 
 func (f *NotExistFile) Error() string {
-if err != nil {
-log.Fatalf("FilePath Error Description -> %v ,FileName: %v , FilePath: %v",f.ErrorDescription,f.Name,f.Path)
-}
-return nil
+	if err != nil {
+		log.Fatalf("FilePath Error Description -> %v ,FileName: %v , FilePath: %v", f.ErrorDescription, f.Name, f.Path)
+	}
+	return nil
 }
 
 //Sistem durması!
-func systemExit(err error) string  {
-if err != nil {
-log.Panicf("%v \n %v","Program beklenmedik şekilde durdu!",time.Now())
-}
-return nil
+func SystemExit(err error) string {
+	if err != nil {
+		log.Panicf("%v \n %v", "Program beklenmedik şekilde durdu!", time.Now())
+	}
+	return nil
 }
 
 //Fatura oluşturma
-func DoesNotCreateBill(err error) string  {
-if err != nil {
-fmt.Printf("%v","Fatura oluşturulurken hata oluştu!")
-}
-return nil
+func DoesNotCreateBill(err error) string {
+	if err != nil {
+		fmt.Printf("%v", "Fatura oluşturulurken hata oluştu!")
+	}
+	return nil
 }
 
 //Veritabanına bağlantı
-func DbConnectionError(err error) string  {
-if err != nil {
-fmt.Printf("%v", "Veritabanına bağlanılamadı!")
-}
-return nil
+func DbConnectionError(err error) string {
+	if err != nil {
+		fmt.Printf("%v", "Veritabanına bağlanılamadı!")
+	}
+	return nil
 }
 
 //Fatura Silinmesi
-func DeleteBillError(err error) string  {
-if err != nil {
-log.Panicf("%v", "Fatura silinirken bir hata oluştu!")
+func DeleteBillError(err error) string {
+	if err != nil {
+		log.Panicf("%v", "Fatura silinirken bir hata oluştu!")
+	}
+	return nil
 }
-return nil
-}
-
-
-
-
-
-
-
-
-
-
