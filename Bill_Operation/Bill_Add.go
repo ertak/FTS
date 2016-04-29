4b617118aed30fdf0e6bc4f87616a3cb034e5cbd
ertak <e.takil@hotmail.com>
2016-04-29 17:36:23 +0300

    Aynı Faturadan eklenemez hatası

--- Bill_Operation/Bill_Add.go
+++ Bill_Operation/Bill_Add.go
@@ -33,21 +33,57 @@ func Add_Bill() {
 		SystemInfo:  System.Info{CreateDate: System.Date(), CreateTime: System.Time(), CreateIP: System.IPControl()},
 	}
 
-	//Error kontrolü yapılmalı bu döneme ait fatura zaten oluşturuldu gibi!!!
+	//Eklenirken aynı döneme ait 2. fatura ekleme kontrolü yapıldı!
+	//Fakat Error verdikten sonra kullanıcıya tekrardan giriş için hak tanınmadı direk uyarı verip program sonlandırıldı!!!!
+	errValueAdd := errors.New("Aynı döneme ait fatura sistemde eklidir!")
 	switch Screen_Question.Billtype {
 	case 1:
-		//Eklenirken aynı döneme ait 2. fatura ekleme kontrolü yapılacak
-		Customer.Account.Bills.Electricity = append(Customer.Account.Bills.Electricity, bill)
-	case 2:
-		Customer.Account.Bills.Gas = append(Customer.Account.Bills.Gas, bill)
-	case 3:
-		Customer.Account.Bills.Water = append(Customer.Account.Bills.Water, bill)
-	case 4:
-		Customer.Account.Bills.Phone = append(Customer.Account.Bills.Phone, bill)
-	case 5:
-		Customer.Account.Bills.Other = append(Customer.Account.Bills.Other, bill)
-	}
+		for _,valueAdd := range Customer.Account.Bills.Electricity {
+			if Screen_Question.BillMonth == valueAdd.Month {
+				fmt.Println(errValueAdd)
+				os.Exit(3)
+			}else {
+				Customer.Account.Bills.Electricity = append(Customer.Account.Bills.Electricity, bill)
+			}
+		}
 
+	case 2:
+		for _,valueAdd := range Customer.Account.Bills.Gas {
+			if Screen_Question.BillMonth == valueAdd.Month {
+				fmt.Println(errValueAdd)
+				os.Exit(3)
+			}else {
+				Customer.Account.Bills.Gas = append(Customer.Account.Bills.Gas, bill)
+			}
+		}
+	case 3:
+		for _,valueAdd := range Customer.Account.Bills.Water {
+			if Screen_Question.BillMonth == valueAdd.Month {
+				fmt.Println(errValueAdd)
+				os.Exit(3)
+			}else{
+				Customer.Account.Bills.Water = append(Customer.Account.Bills.Water, bill)
+			}
+		}
+	case 4:
+		for _,valueAdd := range Customer.Account.Bills.Phone {
+			if Screen_Question.BillMonth == valueAdd.Month {
+				fmt.Println(errValueAdd)
+				os.Exit(3)
+			}else{
+				Customer.Account.Bills.Phone = append(Customer.Account.Bills.Phone, bill)
+			}
+		}
+	case 5:
+		for _,valueAdd := range Customer.Account.Bills.Other {
+			if Screen_Question.BillMonth == valueAdd.Month {
+				fmt.Println(errValueAdd)
+				os.Exit(3)
+			}else{
+				Customer.Account.Bills.Other = append(Customer.Account.Bills.Other, bill)
+			}
+		}
+	}
 	billaddjs, err := json.MarshalIndent(Customer, "", " ")
 	Error.WriteJsonFile(err)
 	fmt.Println(string(billaddjs))
