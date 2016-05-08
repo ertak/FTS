76e5e75a94d939d55922908ee5934f35697ef507
ugurkapl <ugur_kapl@hotmail.com>
2016-05-08 13:22:58 +0300

    recent

--- Bill_Operation/Bill_Add.go
+++ Bill_Operation/Bill_Add.go
@@ -1,15 +1,15 @@
 package Bill_Operation
 
 import (
+	"FTS/Error"
 	"FTS/System_Info"
 	"FTS/model"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"errors"
 	"net/http"
-"FTS/Error"
+	"os"
 )
 
 var Customer Model.User
@@ -20,8 +20,8 @@ func Add_Bill(w http.ResponseWriter, r *http.Request) {
 	frm_billtype := r.FormValue("BillTypeSelect")
 	frm_sonodeme := r.FormValue("drp_sonodeme")
 
-	r.Form["BillTypeSelect"][0]=show_billtype
-	fmt.Println("Esas bakılacak:",r.Form["BillTypeSelect"][0])
+	r.Form["BillTypeSelect"][0] = show_billtype
+	fmt.Println("Esas bakılacak:", r.Form["BillTypeSelect"][0])
 	frm_tutar := r.FormValue("txt_tutar")
 	frm_ay := r.FormValue("dropdown_ay")
 	frm_desc := r.FormValue("description")
@@ -30,7 +30,7 @@ func Add_Bill(w http.ResponseWriter, r *http.Request) {
 	f("Son Ödeme Tarihi:", frm_sonodeme)
 	f("Tutar:", frm_tutar)
 	f("Fatura Kesim Ayı:", frm_ay)
-	f("Açıklama:",frm_desc)
+	f("Açıklama:", frm_desc)
 
 	//Fatura oluşturuldu bildirimi yapılacak!
 
@@ -40,109 +40,104 @@ func Add_Bill(w http.ResponseWriter, r *http.Request) {
 		os.Exit(1)
 	}
 
-		json.Unmarshal(file, &Customer)
+	json.Unmarshal(file, &Customer)
 
-		bill := Model.Bill{
-			Month:       frm_ay,
-			Amount:      frm_tutar,
-			DeadLine:    frm_sonodeme,
-			Description: frm_desc,
-			SystemInfo:  System.Info{CreateDate: System.Date(), CreateTime: System.Time(), CreateIP: System.IPControl()},
-		}
-
-		errValueAdd := errors.New("Aynı döneme ait fatura sistemde eklidir!")
-		switch frm_billtype {
-
-		case "Elektrik":
-			counter1 := 0
-			for _, valueAdd := range Customer.Account.Bills.Electricity {
-				if frm_ay == valueAdd.Month {
-					counter1++
-					if counter1 > 0 {
-						fmt.Fprintln(w,errValueAdd)
-					}
-				}
-			}
-			if counter1 == 0 {
-				Customer.Account.Bills.Electricity = append(Customer.Account.Bills.Electricity, bill)
-
-			}
-
-		case "Doğalgaz":
-			counter2 := 0
-			for _, valueAdd := range Customer.Account.Bills.Gas {
-				if frm_ay == valueAdd.Month {
-					counter2++
-					if counter2 > 0 {
-						fmt.Println(errValueAdd)
-						os.Exit(3)
-					}
-				}
-			}
-			if counter2 == 0 {
-				Customer.Account.Bills.Gas = append(Customer.Account.Bills.Gas, bill)
-			}
-		case "Su":
-			counter3 := 0
-			for _, valueAdd := range Customer.Account.Bills.Water {
-				if frm_ay == valueAdd.Month {
-					counter3++
-					if counter3 > 0 {
-						fmt.Println(errValueAdd)
-						os.Exit(3)
-					}
-				}
-			}
-			if counter3 == 0 {
-				Customer.Account.Bills.Water = append(Customer.Account.Bills.Water, bill)
-			}
-		case "Telefon":
-			counter4 := 0
-			for _, valueAdd := range Customer.Account.Bills.Phone {
-				if frm_ay == valueAdd.Month {
-					counter4++
-					if counter4 > 0 {
-						fmt.Fprintln(w,errValueAdd)
-						os.Exit(3)
-					}
-				}
-			}
-			if counter4 == 0 {
-				Customer.Account.Bills.Phone = append(Customer.Account.Bills.Phone, bill)
-			}
-		case "Diğer":
-			counter5 := 0
-			for _, valueAdd := range Customer.Account.Bills.Other {
-				if frm_ay == valueAdd.Month {
-					counter5++
-					if counter5 > 0 {
-						fmt.Fprintln(w,errValueAdd)
-					}
-				}
-			}
-			if counter5 == 0 {
-				Customer.Account.Bills.Other = append(Customer.Account.Bills.Other, bill)
-
-			}
-		}
-
-		billaddjs, err := json.MarshalIndent(Customer, "", " ")
-		Error.WriteJsonFile(err)
-		//fmt.Println(string(billaddjs))
-
-		ioutil.WriteFile("data/Account.json", billaddjs, 0644)
-
-
-		if err != nil {
-			http.Error(w,err.Error(),http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type","application/json")
-		w.Write(billaddjs)
-
-
+	bill := Model.Bill{
+		Month:       frm_ay,
+		Amount:      frm_tutar,
+		DeadLine:    frm_sonodeme,
+		Description: frm_desc,
+		SystemInfo:  System.Info{CreateDate: System.Date(), CreateTime: System.Time(), CreateIP: System.IPControl()},
 	}
 
+	errValueAdd := errors.New("Aynı döneme ait fatura sistemde eklidir!")
+	switch frm_billtype {
 
+	case "Elektrik":
+		counter1 := 0
+		for _, valueAdd := range Customer.Account.Bills.Electricity {
+			if frm_ay == valueAdd.Month {
+				counter1++
+				if counter1 > 0 {
+					fmt.Fprintln(w, errValueAdd)
+				}
+			}
+		}
+		if counter1 == 0 {
+			Customer.Account.Bills.Electricity = append(Customer.Account.Bills.Electricity, bill)
 
+		}
+
+	case "Doğalgaz":
+		counter2 := 0
+		for _, valueAdd := range Customer.Account.Bills.Gas {
+			if frm_ay == valueAdd.Month {
+				counter2++
+				if counter2 > 0 {
+					fmt.Println(errValueAdd)
+					os.Exit(3)
+				}
+			}
+		}
+		if counter2 == 0 {
+			Customer.Account.Bills.Gas = append(Customer.Account.Bills.Gas, bill)
+		}
+	case "Su":
+		counter3 := 0
+		for _, valueAdd := range Customer.Account.Bills.Water {
+			if frm_ay == valueAdd.Month {
+				counter3++
+				if counter3 > 0 {
+					fmt.Println(errValueAdd)
+					os.Exit(3)
+				}
+			}
+		}
+		if counter3 == 0 {
+			Customer.Account.Bills.Water = append(Customer.Account.Bills.Water, bill)
+		}
+	case "Telefon":
+		counter4 := 0
+		for _, valueAdd := range Customer.Account.Bills.Phone {
+			if frm_ay == valueAdd.Month {
+				counter4++
+				if counter4 > 0 {
+					fmt.Fprintln(w, errValueAdd)
+					os.Exit(3)
+				}
+			}
+		}
+		if counter4 == 0 {
+			Customer.Account.Bills.Phone = append(Customer.Account.Bills.Phone, bill)
+		}
+	case "Diğer":
+		counter5 := 0
+		for _, valueAdd := range Customer.Account.Bills.Other {
+			if frm_ay == valueAdd.Month {
+				counter5++
+				if counter5 > 0 {
+					fmt.Fprintln(w, errValueAdd)
+				}
+			}
+		}
+		if counter5 == 0 {
+			Customer.Account.Bills.Other = append(Customer.Account.Bills.Other, bill)
+
+		}
+	}
+
+	billaddjs, err := json.MarshalIndent(Customer, "", " ")
+	Error.WriteJsonFile(err)
+	//fmt.Println(string(billaddjs))
+
+	ioutil.WriteFile("data/Account.json", billaddjs, 0644)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json;text/html; charset=utf-8")
+	w.Write(billaddjs)
+
+}
