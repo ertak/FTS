4aa8009702a8252f564cba140877c02ea818de43
ertak <e.takil@hotmail.com>
2016-05-20 16:24:44 +0300

    update

--- web/getMonth.js
+++ web/getMonth.js
@@ -1,16 +1,48 @@
-
+?
 $(document).ready(function () {
 //1. combonun click fonksiyonu alýnacak!
-  $('#get-data').click(function () {
-    var showData = $('#show-data');
+  $('#get-deldata').click(function () {
+    var showData = $('#del-data');
+
+   var e = document.getElementById("billSelect");
+   var strUser = e.options[e.selectedIndex].text;
+    var res = String(strUser);
+   console.log(res)
 
     $.getJSON('Account2.json', function (data) {
       console.log(data);
+    var Months;
 
-      var Months = data.Account.Bills.Water.map(function (item) {
-        return  item.Month;
-      });
+      switch (res)
+      {
+      case "Su":
+               Months = data.Account.Bills.Water.map(function (item) {
+              return  item.Month;
+            });
+           break;
+      case "Elektrik":
+               Months = data.Account.Bills.Electricity.map(function (item) {
+                    return  item.Month;
+                  });
+            break;
+       case "Telefon":
+               Months = data.Account.Bills.Phone.map(function (item) {
+                      return  item.Month;
+                              });
+            break;
+        case "Diðer":
+               Months = data.Account.Bills.Other.map(function (item) {
+                      return  item.Month;
+                              });
+            break;
+        case "Doðalgaz":
+               Months = data.Account.Bills.Gas.map(function (item) {
+                      return  item.Month;
+                               });
+            break;
 
+
+      }
       showData.empty();
 
       if (Months.length) {
@@ -24,30 +56,66 @@ $(document).ready(function () {
 
     showData.text('Loading the JSON file.');
   });
+
+
+  $('#get-showdata').click(function () {
+      var showData2 = $('#show-data');
+
+     var e = document.getElementById("show_bill_select");
+     var strUser = e.options[e.selectedIndex].text;
+      var res = String(strUser);
+     console.log(res)
+
+      $.getJSON('Account2.json', function (data) {
+        console.log(data);
+      var Months;
+
+        switch (res)
+        {
+        case "Su":
+               Months = data.Account.Bills.Water.map(function (item) {
+                return  item.Month;
+              });
+             break;
+        case "Elektrik":
+               Months = data.Account.Bills.Electricity.map(function (item) {
+                      return  item.Month;
+                    });
+              break;
+         case "Telefon":
+                Months = data.Account.Bills.Phone.map(function (item) {
+                        return  item.Month;
+                                });
+              break;
+          case "Diðer":
+                Months = data.Account.Bills.Other.map(function (item) {
+                        return  item.Month;
+                                });
+              break;
+          case "Doðalgaz":
+                Months = data.Account.Bills.Gas.map(function (item) {
+                        return  item.Month;
+                                 });
+              break;
+
+
+        }
+        showData.empty();
+
+        if (Months.length) {
+          console.log(Months.length)
+          console.log(data)
+          var content = '<option>' + Months.join('</option><option>') + '</option>';
+          var list = $('<select name="dropdown_ay" id="dropmonth"/>').html(content);
+          showData.append(list);
+        }
+      });
+
+      showData.text('Loading the JSON file.');
+    });
+
+
+
 });
 
 
-
-/*$(document).ready(function () {
-  $('#get-data').click(function () {
-    var showData = $('#show-data');
-
-    $.getJSON('example.json', function (data) {
-      console.log(data);
-
-      var items2 = data.items.map(function (item) {
-        return item.key + ': ' + item.value;
-      });
-
-      showData.empty();
-
-      if (items2.length) {
-        var content = '<li>' + items2.join('</li><li>') + '</li>';
-        var list = $('<ul />').html(content);
-        showData.append(list);
-      }
-    });
-
-    showData.text('Loading the JSON file.');
-  });
-});*/
